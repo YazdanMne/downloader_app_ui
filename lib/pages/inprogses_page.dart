21f5683a95efcc3e60d0d349c8dc0fca150cc37f
YazdanMne <yazda.manouchehri1384@gmail.com>
2023-08-17 05:41:31 -0400

    done 8/17/2023

--- lib/pages/inprogses_page.dart
+++ lib/pages/inprogses_page.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../constants/my_color.dart';
 
 class InProgsesPage extends StatelessWidget {
   const InProgsesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('in Progses page'),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    SolidColor.orange200,
+                    SolidColor.orange50,
+                    SolidColor.orange50,
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 82.h),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'In Progress',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: SolidColor.rhino500,
+                      fontSize: 16.sp,
+                      fontFamily: 'MB',
+                      height: 1.62.h,
+                      letterSpacing: -0.16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 128.h),
+              child: Container(
+                width: double.infinity,
+                height: 684.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.r),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 24.h, left: 24.w, right: 24.w),
+                      child: Center(
+                        child: Column(
+                          children: [],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
